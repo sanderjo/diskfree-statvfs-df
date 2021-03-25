401f571e951ff0ee8ca1b9361ab8b0dfa1a7cf38
Sander Jonkers <sander.jonkers+github@gmail.com>
2021-03-25 14:25:29 +0100

    statfs32(): return round() MB values ... for real now

--- diskfree_statvfs_df.py
+++ diskfree_statvfs_df.py
@@ -91,8 +91,8 @@ def disk_free_clib_statfs32(directory):
 	root_volume = create_string_buffer(str.encode(directory))
 	fs_info = statfs32()
 	result = kern.statfs(root_volume, byref(fs_info)) # you have to call this to get fs_info filled out
-	disk_size_MB = fs_info.f_blocks * fs_info.f_bsize / 1024**2
-	free_size_MB = fs_info.f_bavail * fs_info.f_bsize / 1024**2
+	disk_size_MB = round(fs_info.f_blocks * fs_info.f_bsize / 1024**2)
+	free_size_MB = round(fs_info.f_bfree  * fs_info.f_bsize / 1024**2)
 	return disk_size_MB, free_size_MB
 
 
